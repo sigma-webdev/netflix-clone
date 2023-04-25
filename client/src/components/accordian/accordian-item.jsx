d7dd5797ad95c306b4b9b8d274c47f5af93dda83
sub1120 <subhamsahu1120@gmail.com>
2023-04-25 17:00:02 +0530

    fixed accordian

--- client/src/components/accordian/accordian-item.jsx
+++ client/src/components/accordian/accordian-item.jsx
@@ -1,24 +1,64 @@
-const AccordianItem = ({ isActive, children, accordianHandler, id }) => {
-  const tabHandler = (accordianHandler, id) => {
+const AccordianItem = ({
+  accordianHandler,
+  isActive,
+  question,
+  answer,
+  id,
+}) => {
+  const tabHandler = () => {
     if (!isActive) {
       accordianHandler(id);
     } else {
-      accordianHandler(0);
+      accordianHandler(-1);
     }
   };
 
   return (
-    <li onClick={tabHandler}>
-      <h2>
-        <button>What is Netflix?</button>
+    <li onClick={(id) => tabHandler()}>
+      <h2 className="flex justify-between p-6 text-2xl bg-[#132144] hover:bg-[#223362]">
+        <button>{question}</button>
+        <span>
+          {!isActive ? (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-10 h-10"
+              >
+                {" "}
+                :
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            </>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-10 h-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </>
+          )}
+        </span>
       </h2>
-      <p className={`${isActive ? "" : "hidden"}`}>
-        Netflix is a streaming service that offers a wide variety of
-        award-winning TV shows, movies, anime, documentaries and more – on
-        thousands of internet-connected devices. <br /> You can watch as much as
-        you want, whenever you want, without a single ad – all for one low
-        monthly price. There's always something new to discover, and new TV
-        shows and movies are added every week!
+      <p className={`${isActive ? "bg-[#132144] p-6 text-2xl" : "hidden"} `}>
+        {answer}
       </p>
     </li>
   );
