4d3f35718e78a4cb7d7ae791b6a8e0784c9a5922
Nasikh CL <nasikcl@gmail.com>
2023-04-26 17:55:56 +0530

    sign up page step 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./Home";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <main>
       {/* <Home />   */}
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </main>
   );
 }
