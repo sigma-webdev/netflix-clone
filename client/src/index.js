6c04a0f41195c6d6b387e76e6020da9da72c0c25
sub1120 <subhamsahu1120@gmail.com>
2023-04-26 15:08:37 +0530

    added react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import App from "./App";
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    }
+  ]);
+  
+  
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( <RouterProvider router={router} />);
