efd753932422eba10632929def66f2c9be11a49c
sub1120 <subhamsahu1120@gmail.com>
2023-05-05 10:34:08 +0530

    seperated plus and coross icon to components

--- client/src/components/accordian/AccordianItem.jsx
+++ client/src/components/accordian/AccordianItem.jsx
@@ -1,3 +1,6 @@
+import CrossIcon from "../icon/CrossIcon";
+import PlusIcon from "../icon/PlusIcon";
+
 const AccordianItem = ({
   accordianHandler,
   isActive,
@@ -17,9 +20,7 @@ const AccordianItem = ({
     <li onClick={tabHandler}>
       <h2 className="flex justify-between p-6 text-2xl bg-[#132144] hover:bg-[#223362]">
         <button>{question}</button>
-        <span>
-          {!isActive ? <AccordianOpenIcon /> : <AccordianCloseIcon />}
-        </span>
+        <span>{!isActive ? <PlusIcon /> : <CrossIcon />}</span>
       </h2>
       <p
         className={`${
@@ -31,49 +32,6 @@ const AccordianItem = ({
         {answer}
       </p>
     </li>
-  );
-};
-
-const AccordianOpenIcon = () => {
-  return (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-10 h-10"
-      >
-        :
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 4.5v15m7.5-7.5h-15"
-        />
-      </svg>
-    </>
-  );
-};
-
-const AccordianCloseIcon = () => {
-  return (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-10 h-10"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
-    </>
   );
 };
 
