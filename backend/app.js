7ee734ad58feb182e7bd18fba0b4ec671e3275dd
mtouthang <huebart16@gmail.com>
2023-05-19 18:39:40 +0530

    fix: fixed the server crash issue

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
+
 const authRouter = require("./router/authRouter");
 const morgan = require("morgan");
 const fileUpLoad = require("express-fileupload");
 
-const errorHandler = require("./middleware/errotHandler.js");
 const contentRoute = require("./router/contentRouter");
-=======
-const authRouter = require("./router/authRouter.js");
 const errorHandler = require("./middleware/errorHandler.js");
->>>>>>> f0bf0c092da2de8ca7a60a678707d747d86cdb49
 
 // database connection
 require("./config/databaseConnection");
 
-<<<<<<< HEAD
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,7 +24,6 @@ app.use(
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/content", contentRoute);
-=======
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // routes
@@ -37,7 +31,6 @@ app.use("/api/v1/auth", authRouter);
 app.get("/", (req, res) => {
   return res.status(200).json({ data: "netflix server" });
 });
->>>>>>> f0bf0c092da2de8ca7a60a678707d747d86cdb49
 
 // errorhandler
 app.use(errorHandler);
