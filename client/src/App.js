a527e622a571d87d00a13b20dfe4217050ff9a2e
sub1120 <subhamsahu1120@gmail.com>
2023-05-19 18:54:29 +0530

    fixed browse page route

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
-      <Route path="/browse" element={<Browse />}></Route>
+      <Route path="/browse" element={<Browse />} />
     </Routes>
   );
 }
