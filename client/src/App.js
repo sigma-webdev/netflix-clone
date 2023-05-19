5af9f54c9459ede8deed8d67cc2cbdb804726fbc
sub1120 <subhamsahu1120@gmail.com>
2023-05-19 18:53:24 +0530

    fixed browse page route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Browse from "./pages/Browse";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
+      <Route path="/browse" element={<Browse />}></Route>
     </Routes>
   );
 }
