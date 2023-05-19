d4b41e37db1a0aa98630abd73d77748c79e875fc
sub1120 <subhamsahu1120@gmail.com>
2023-05-19 17:05:51 +0530

    updated layout and browsepage

--- client/src/data.js
+++ client/src/data.js
@@ -1,4 +1,72 @@
 import samplePoster from "./assets/images/content-poster.jpg";
+import show1 from "./assets/images/show1.png";
+import show2 from "./assets/images/show2.png";
+import show3 from "./assets/images/show3.png";
+import show4 from "./assets/images/show4.png";
+
+export const faqs = [
+  {
+    id: 1,
+    question: "How much does Netflix cost?",
+    answer:
+      "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.  You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
+  },
+  {
+    id: 2,
+    question: "Where can I watch?",
+    answer:
+      "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.",
+  },
+  {
+    id: 3,
+    question: "What can I watch on Netflix?",
+    answer:
+      "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+  },
+  {
+    id: 4,
+    question: "How do I cancel?",
+    answer:
+      "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+  },
+  {
+    id: 5,
+    question: "Is Netflix good for kids?",
+    answer:
+      "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+  },
+];
+
+export const features = [
+  {
+    id: 1,
+    featureHeading: "Enjoy on your TV.",
+    featureImage: show1,
+    aboutFeature:
+      "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+  },
+  {
+    id: 2,
+    featureHeading: "Watch everywhere.",
+    featureImage: show2,
+    aboutFeature:
+      "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+  },
+  {
+    id: 3,
+    featureHeading: "Create profiles for children.",
+    featureImage: show3,
+    aboutFeature:
+      "Send children on adventures with their favourite characters in a space made just for them—free with your membership.",
+  },
+  {
+    id: 4,
+    featureHeading: "Download your shows to watch offline.",
+    featureImage: show4,
+    aboutFeature:
+      "Save your favourites easily and always have something to watch.",
+  },
+];
 
 export const GENERES = [
   {
--- client/src/pages/Browse.jsx
+++ client/src/pages/Browse.jsx
@@ -1,27 +1,28 @@
 import Crousal from "../components/crousal/Crousal";
-import BrowseHeader from "../components/layout/BrowseHeader";
+import Layout from "../components/layout/Layout";
 
 import { GENERES, content } from "../data";
 
 const Browse = () => {
   return (
-    <div className="bg-netflix-black text-white">
-      <BrowseHeader></BrowseHeader>
-      <div className="px-8 space-y-5">
-        {GENERES.map((currentGenere) => {
-          const filteredContent = content.filter(
-            (item) => item.genre === currentGenere.name
-          );
+    <Layout isLogin={true}>
+      <div className="text-white bg-netflix-blue">
+        <div className="px-8 space-y-5">
+          {GENERES.map((currentGenere) => {
+            const filteredContent = content.filter(
+              (item) => item.genre === currentGenere.name
+            );
 
-          return (
-            <div key={currentGenere.id}>
-              <h4 className="relative my-2">{currentGenere.name}</h4>
-              <Crousal content={filteredContent}></Crousal>
-            </div>
-          );
-        })}
+            return (
+              <div key={currentGenere.id}>
+                <h4 className="relative mb-2">{currentGenere.name}</h4>
+                <Crousal content={filteredContent}></Crousal>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
