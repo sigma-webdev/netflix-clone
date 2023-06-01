7ceefd28babe21e8ccb4cc49044f15852ff64e48
Nasikh CL <nasikcl@gmail.com>
2023-06-01 16:14:00 +0530

    working on admin page navbar

--- client/src/components/AdminNav.jsx
+++ client/src/components/AdminNav.jsx
@@ -3,17 +3,17 @@ import { HiHome, HiUserGroup, HiServer } from "react-icons/hi";
 const AdminNav = () => {
   return (
     <div className="p-2 w-2/12 h-screen bg-slate-800 text-white">
-      <h3 className="mb-12">Netflix</h3>
+      <h3 className="mb-12">Netflix Admin</h3>
       <ul>
-        <li className="flex items-center mb-4 border border-white py-2 px-1 rounded">
+        <li className="flex items-center mb-4 border border-white py-2 px-1 rounded cursor-pointer">
           <HiHome />
           <p className="ml-2">Dashboard</p>
         </li>
-        <li className="flex items-center mb-4 border border-white py-2 px-1 rounded">
+        <li className="flex items-center mb-4  border-white py-2 px-1 rounded cursor-pointer">
           <HiUserGroup />
           <p className="ml-2">Manage Users</p>
         </li>
-        <li className="flex items-center mb-4 border border-white py-2 px-1 rounded">
+        <li className="flex items-center mb-4  border-white py-2 px-1 rounded cursor-pointer">
           <HiServer />
           <p className="ml-2">Manage Contents</p>
         </li>
