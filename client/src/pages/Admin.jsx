da46064c91afc4263f83d5a351a672d49fdae0de
Nasikh CL <nasikcl@gmail.com>
2023-06-12 15:47:50 +0530

    feature: working on admin content view page.(not completed). integrearted with backend api

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -4,13 +4,15 @@ import AdminManageUsers from '../components/AdminManageUsers'
 // import AdminDashboard from '../components/AdminDashboard'
 import { Routes, Route } from "react-router-dom";
 import AdminManageContents from '../components/AdminManageContents';
+import AdminContentView from '../components/AdminContentView';
 const Admin = () => {
   return (
     <div className='flex flex-wrap'>
       <AdminNav />
       <Routes>
-        <Route path="/manageusers" element={<AdminManageUsers />} />
-        <Route path="/managecontents" element={<AdminManageContents />} />
+        <Route path="manageusers/" element={<AdminManageUsers />} />
+        <Route path="managecontents/" element={<AdminManageContents />} />
+        <Route path="managecontents/:id" element={<AdminContentView />} />
       </Routes>
 
 
