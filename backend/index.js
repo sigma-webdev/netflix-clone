a7d6a2e0cf85c89d06edb917cdfb32ab67e932ba
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-06-27 12:19:49 +0530

    refactor: import order & log msg on server startup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
-const PORT = 8081 || process.env.PORT;
+
 const app = require("./app.js");
 
+const PORT = 8081 || process.env.PORT;
+
 app.listen(PORT, () => {
-  console.log(`server listening at ${PORT}`);
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
