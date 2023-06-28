ecbc0d3474ae21d29fd78a171afb01d99edae79e
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-06-28 17:37:21 +0530

    fix: updated plan form with layout and removed from components

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import SignUp from "../pages/SignUp/SignUp.js";
+import PlanForm from "../pages/SignUp/PlanForm.js";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignUp />,
     children: [],
+  },
+  {
+    path: "/signup/planform",
+    element: <PlanForm />,
   },
 ]);
 
