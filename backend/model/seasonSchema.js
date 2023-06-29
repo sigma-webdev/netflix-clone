abce7a7965524fe8acab9b1083fcd290a3d59444
MTouthang <huebart16@gmail.com>
2023-06-29 20:01:33 +0530

    update season schema

--- backend/model/seasonSchema.js
+++ backend/model/seasonSchema.js
@@ -4,26 +4,14 @@ const { Schema } = mongoose;
 const seasonSchema = new Schema(
   {
     seasonName: {
-      type: String,
+      type: Number,
       required: [true, "Season name is Required"],
-      minLength: [5, "Season name must be at least 5 characters"],
-      maxLength: [40, "Season name must be less than 40 characters"],
     },
     seasonSummary: {
       type: String,
       minLength: [15, "Content description must be at least 15 characters"],
       maxLength: [250, "Content description must be less than 250 characters "],
     },
-    thumbnail: [
-      {
-        seasonPublicId: {
-          type: String,
-        },
-        seasonSecureUrl: {
-          type: String,
-        },
-      },
-    ],
     episodes: [
       {
         type: mongoose.Schema.Types.ObjectId,
