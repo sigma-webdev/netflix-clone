7967a25e92df695100a0e491085fdc07a6122ee4
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-06-30 16:57:28 +0530

    fix: rename secrete to jet_secret & added jet_expiry in .env with some other minor changes

--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
 const crypto = require("crypto");
-const contentModel = require("./contentSchema");
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -15,7 +13,7 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      required: [true, "user email is required"],
+      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
     },
@@ -33,17 +31,32 @@ const userSchema = new Schema(
       enum: ["PREMIUM", "STANDARD", "BASIC", "MOBILE", "NONE"],
     },
     watchHistory: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Content", select: false },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Content",
+        select: false,
+      },
     ],
     watchList: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Content", select: false },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Content",
+        select: false,
+      },
     ],
     subscription: {
       id: String,
       status: String,
     },
-    role: { type: String, default: "USER", enum: ["ADMIN", "USER"] },
-    forgotPasswordExpiryDate: { type: Date, select: false },
+    role: {
+      type: String,
+      default: "USER",
+      enum: ["ADMIN", "USER"],
+    },
+    forgotPasswordExpiryDate: {
+      type: Date,
+      select: false,
+    },
   },
   { timestamps: true }
 );
@@ -60,13 +73,14 @@ userSchema.pre("save", async function (next) {
 userSchema.methods = {
   getForgotPasswordToken() {
     const forgotToken = crypto.randomBytes(20).toString("hex");
+
     //step 1 - save to DB
     this.forgotPasswordToken = crypto
       .createHash("sha256")
       .update(forgotToken)
       .digest("hex");
 
-    /// forgot password expiry date
+    // forgot password expiry date
     this.forgotPasswordExpiryDate = new Date(Date.now() + 20 * 60 * 1000); // 20min
 
     //step 2 - return values to user
@@ -75,12 +89,13 @@ userSchema.methods = {
   generateJwtToken() {
     const token = JWT.sign(
       { id: this._id, email: this.email, role: this.role },
-      process.env.SECRETE,
-      { expiresIn: 24 * 60 * 60 * 1000 } //24
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRY }
     );
     return token;
   },
 };
 
 const userModel = mongoose.model("User", userSchema);
+
 module.exports = userModel;
