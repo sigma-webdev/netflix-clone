36e298759d7044c0fd11cce02d6957096d72bddb
sub1120 <subhamsahu1120@gmail.com>
2023-06-30 15:53:02 +0530

    working on browse page shimmer and fixing UI

--- client/src/components/card/PreviewCard.jsx
+++ client/src/components/card/PreviewCard.jsx
@@ -36,8 +36,6 @@ const PreviewCard = ({
     dispatch(dislikeContent({ contentId, userId: "64789b082f388ccff2e33eaa" }));
   };
 
-  console.log(contentDuration);
-
   return (
     <div className="tranistion my-8 w-48 scale-100 rounded bg-netflix-black drop-shadow-lg duration-300 ease-in-out hover:z-10 hover:ml-10 hover:scale-125 hover:opacity-100 md:w-64">
       {/* preview video*/}
@@ -84,7 +82,7 @@ const PreviewCard = ({
 
       {isOpenDetails &&
         createPortal(
-          <div className="fixed top-0 z-50 h-full w-full bg-black/60 pt-[4%]">
+          <div className="fixed top-0 z-50 flex h-full w-full items-center bg-black/60  pt-[4%]">
             <DetailsCard
               name={name}
               description={description}
