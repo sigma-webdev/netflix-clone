fd7702061848508b1d8fc9425ca183681ac8d0ec
PRABIR07 <uic.19mca1007@gmail.com>
2023-07-03 16:44:06 +0530

    feature: admin dashboard UI

--- client/src/components/admin/AdminDashboard.jsx
+++ client/src/components/admin/AdminDashboard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import DashboardCard from "../card/DashboardCard";
+import Barchart from "../../Charts/Barchart";
+import PieChart from "../../Charts/PieChart";
+import StackedBarChart from "../../Charts/StackedBarChart";
 
 const AdminDashboard = () => {
   return (
-    <>
-      <h1>Admin Dashboard</h1>
-    </>
-  )
+    <div className=" text-zinc-100 w-full">
+      <div className="flex flex-wrap justify-evenly items-center lg:flex-nowrap ">
+        <DashboardCard />
+        <PieChart />
+      </div>
+      <Barchart />
+    </div>
+  );
 };
 
 export default AdminDashboard;
