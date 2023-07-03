677c5603c213c651d114b02b593f5c31289e7bdf
Mangesh Thakre <mangeshthakre933@gmail.com>
2023-07-03 12:37:27 +0530

    added error handler for duplicate key

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -2,6 +2,14 @@ const errorHandler = (error, req, res, next) => {
   const errorStatusCode = error.statusCode || 500;
   const errorMessage = error.message || "Internal Server error :(";
 
+  if (error.code === 11000) {
+    return res.status(errorStatusCode).json({
+      statusCode: errorStatusCode,
+      success: false,
+      message: "Duplicate key error",
+    });
+  }
+
   if (error.name === "CastError") {
     return res.status(errorStatusCode).json({
       success: false,
