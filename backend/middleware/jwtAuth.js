fb642a22b33e92be8343949e16cc14ad684e83ec
Mangesh Thakre <mangeshthakre933@gmail.com>
2023-07-04 12:26:06 +0530

    update the jwt response object and send the userObject without password field during sign

--- backend/middleware/jwtAuth.js
+++ backend/middleware/jwtAuth.js
@@ -4,12 +4,22 @@ const jwtAuth = (req, res, next) => {
   const token = (req.cookies && req.cookies.token) || null;
 
   if (!token) {
-    return res.status(400).json({ success: false, message: "NOT authorized" });
+    return res.status(400).json({
+      statusCode: 400,
+      success: false,
+      message: "NOT authorized",
+      data: null,
+    });
   }
 
   JWT.verify(token, process.env.JWT_SECRET, function (error, payload) {
     if (error) {
-      return res.status(400).json({ success: false, message: error.message });
+      return res.status(400).json({
+        statusCode: 400,
+        success: false,
+        message: error.message,
+        data: null,
+      });
     } else {
       req.user = { id: payload.id, email: payload.email, role: payload.role };
     }
