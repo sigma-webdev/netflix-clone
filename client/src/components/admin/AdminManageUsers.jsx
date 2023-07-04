5cdbc09e3706a1229414157816bee066e0847fe0
maheshsarade001 <maheshsarade001@gmail.com>
2023-07-04 11:55:47 +0530

    added loader

--- client/src/components/admin/AdminManageUsers.jsx
+++ client/src/components/admin/AdminManageUsers.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllUsers } from "../../store/userSlice";
+import { getAllUsers, getUserById } from "../../store/userSlice";
 import { HiSearch } from "react-icons/hi";
+import AdminPageLoader from "../loader/AdminPageLoader";
 
 const AdminManageUsers = () => {
 
@@ -9,6 +10,9 @@ const AdminManageUsers = () => {
 
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => state.user.allUsers);
+  const isDataLoading = useSelector((state)=> state.user.loading)
+  const getUser= useSelector((state)=>state.user.userById)
+  console.log(getUser, "abcd");
   const [isOpen, setIsOpen] = useState(false);
   const [page, setPage] = useState(1) 
   const [searchText, setSearchText] = useState("")
@@ -26,6 +30,7 @@ const AdminManageUsers = () => {
 
 
   const prevPage = () =>{
+    
     if(allUsers?.data?.previous === undefined){
       return;
     
@@ -51,10 +56,17 @@ const AdminManageUsers = () => {
     setSearchText("")
 
   }
+
+  const userById = (id)=>{
+    toggleModal(true)
+    console.log(id);
+    dispatch(getUserById(id))
+  }
  
 
   return (
     <>
+    {/* <AdminPageLoader/> */}
       {isOpen && (
         <div className="absolute flex h-full w-full items-center justify-center bg-gray-600 bg-opacity-50">
           <div className="relative w-96 rounded-lg bg-white px-4 py-12">
@@ -79,10 +91,11 @@ const AdminManageUsers = () => {
           </div>
         </div>
       )}
+      {isDataLoading? <AdminPageLoader/> :(
       <div className=" max-h-screen w-10/12 overflow-y-scroll bg-slate-100 py-10">
         <div className="flex justify-between w-5/6 mx-auto">
-        <h3 className="text-white bg-[#E50914] px-3 rounded-t-md">Manage Users</h3>
-        <div className="flex justify-between border-2 border-[#E50914] items-center w-1/3 bg-white ">
+        <h3 className="text-white bg-red-600 px-3 rounded-t-md">Manage Users</h3>
+        <div className="flex justify-between border-2 border-red-600 items-center w-1/3 bg-white ">
           <form onSubmit={getSearch} className="w-full">
           <input onChange={(e)=>{setSearchText(e.target.value)}} value={searchText} type="text" placeholder="Search here" className=" px-2 outline-none w-full" />
           </form>
@@ -91,7 +104,7 @@ const AdminManageUsers = () => {
         </div>
         <table className="mx-auto w-5/6 table-auto overflow-scroll  text-gray-200">
           <thead className="text-left">
-            <tr className="bg-[#E50914] text-white">
+            <tr className="bg-red-600 text-white">
               <th className="px-4 py-2">S. No</th>
               <th className="px-4 py-2">Name</th>
               <th className="px-4 py-2">Email</th>
@@ -113,10 +126,10 @@ const AdminManageUsers = () => {
                     <td className="px-4 py-3">{user?.email}</td>
                     <td className="px-4 py-2">
                       <div
-                        onClick={toggleModal}
-                        className=" rounded bg-[#E50914] py-2 text-center font-bold text-white hover:bg-[#d4252e]"
+                        onClick={()=>{userById(user._id)}} 
+                        className=" rounded bg-red-600 py-2 text-center font-bold text-white hover:bg-red-500"
                       >
-                        View
+                        View{user._id}
                       </div>
                     </td>
                   </tr>
@@ -125,13 +138,14 @@ const AdminManageUsers = () => {
           </tbody>
         </table>
         <div className="flex justify-between w-10/12 my-5 mx-auto">
-                <button className={allUsers?.data?.previous === undefined ? "bg-[#e5091451]  text-white  py-1 px-2 cursor-not-allowed":"bg-[#E50914] hover:bg-[#d4252e] text-white  py-1 px-2"} onClick={prevPage}>Previous Page</button>
-                <div className=" px-[10px] border-2 border-[#e509144d] text-[#E50914] text-xl font-bold rounded-full ">
+                <button className={allUsers?.data?.previous === undefined ? "bg-red-300  text-white  py-1 px-2 cursor-not-allowed":"bg-red-600 hover:bg-red-500 text-white  py-1 px-2"} onClick={prevPage}>Previous Page</button>
+                <div className=" px-[10px] border-2 border-red-400 text-red-600 text-xl font-bold rounded-full ">
                  {page}
                 </div>
-                <button onClick={nextPage} className={allUsers?.data?.next === undefined? "bg-[#e5091451]  text-white  py-1 px-2 cursor-not-allowed":"bg-[#E50914] hover:bg-[#d4252e] text-white  py-1 px-4"}>Next Page</button>
+                <button onClick={nextPage} className={allUsers?.data?.next === undefined? "bg-red-300  text-white  py-1 px-2 cursor-not-allowed":"bg-red-600 hover:bg-red-500 text-white  py-1 px-4"}>Next Page</button>
               </div>
       </div>
+      )}
     </>
   );
 };
