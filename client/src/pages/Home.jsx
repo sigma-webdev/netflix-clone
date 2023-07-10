869d10435e224a94cbe924d47962a6a7421dfc12
sub1120 <subhamsahu1120@gmail.com>
2023-07-10 15:25:26 +0530

    seperated get started from faq section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import validator from "email-validator";
 import toast from "react-hot-toast";
@@ -19,9 +19,7 @@ const Home = () => {
   const dispatch = useDispatch();
   const [activeItem, setActiveItem] = useState(-1);
   const navigate = useNavigate();
-  const { isLoggedIn: IS_LOGGED_IN, loading } = useSelector(
-    (state) => state.auth
-  );
+  const { IS_LOGGED_IN, loading } = useSelector((state) => state.auth);
   const USER_DATA = useSelector((state) => state.auth.userData);
 
   // handler for accordion
@@ -45,7 +43,7 @@ const Home = () => {
     <Layout>
       <div className="relative bg-[#000000] text-white">
         {/* hero section */}
-        <section className="m-auto w-full">
+        <section className="mx-auto w-full">
           <div className="bg-repeat-no h-[35rem] bg-netflix-home bg-cover md:h-[45rem]">
             <div
               className="bg-[rgb(0 0 0 / 40%)] flex h-full w-full items-center justify-center"
@@ -102,12 +100,12 @@ const Home = () => {
                           </label>
                         </div>
                         <div>
-                          <Link
-                            to="/signup"
-                            className="flex h-14 items-center rounded bg-red-600 px-2 text-2xl  font-bold text-white hover:bg-red-700 md:px-4 lg:px-6"
+                          <button
+                            className="md:text-md flex h-10 items-center rounded bg-red-600  px-2 text-sm font-bold text-white hover:bg-red-700  md:h-12 md:px-4 lg:h-14 lg:px-6 lg:text-lg"
+                            type="submit"
                           >
                             Get Started <MdKeyboardArrowRight />
-                          </Link>
+                          </button>
                         </div>
                       </form>
                     )}
@@ -135,7 +133,7 @@ const Home = () => {
         </section>
 
         {/* faq section */}
-        <section className="mx-auto max-w-[90%] space-y-4 py-10 md:max-w-[85%] lg:max-w-[80%]">
+        <section className="mx-auto max-w-[90%] space-y-4 py-5 md:max-w-[85%] lg:max-w-[80%]">
           <h2 className="text-xl font-bold md:text-3xl lg:text-5xl">
             Frequently Asked Questions
           </h2>
@@ -153,49 +151,60 @@ const Home = () => {
               );
             })}
           </Accordian>
+        </section>
 
-          <>
-            <p className="text-2xl">
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </p>
-            <div className="flex space-x-4">
-              <div className="max-w-80 group relative z-0 mb-6 rounded border-2 bg-black text-sm opacity-75 md:w-96">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="peer block w-full appearance-none bg-transparent p-4 text-sm focus:border-blue-600 focus:outline-none focus:ring-0"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  htmlFor="floating_email"
-                  className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform px-4 pb-2 text-xl text-slate-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:font-medium"
-                >
-                  Email address
-                </label>
-              </div>
-              <div>
+        {/* get started */}
+        <section className="mx-auto max-w-[90%] space-y-4 py-5 md:max-w-[85%] lg:max-w-[80%]">
+          <p className="text-lg md:text-xl lg:text-2xl">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+
+          {loading ? (
+            "loading"
+          ) : (
+            <div className="flex">
+              {IS_LOGGED_IN && USER_DATA.plan === "NONE" ? (
                 <Link
-                  to="/signup"
-                  className="flex h-14 items-center rounded bg-red-600 px-2 text-2xl  font-bold text-white hover:bg-red-700 md:px-4 lg:px-6"
+                  to="/signup/choose"
+                  className="mt-5 rounded bg-red-600 px-4 py-2 align-middle text-lg font-medium text-white hover:bg-red-700"
                 >
-                  Get Started <MdKeyboardArrowRight />
+                  Finish signUp
+                  <MdOutlineKeyboardArrowRight />
                 </Link>
-              </div>
+              ) : (
+                <form
+                  className="flex w-[80%] flex-col gap-2 md:flex-row md:items-center md:gap-4 lg:gap-6"
+                  onSubmit={(e) => handleIsUserExist(e)}
+                >
+                  <div className="group relative z-0 w-full rounded border-2 bg-black text-sm opacity-75 sm:w-96">
+                    <input
+                      type="email"
+                      name="email"
+                      id="floating_email"
+                      className="peer block w-full appearance-none bg-transparent p-3 focus:border-blue-600 focus:outline-none focus:ring-0 md:p-4 lg:p-5"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      htmlFor="floating_email"
+                      className="md:text-md absolute top-2 -z-10 origin-[0] -translate-y-6 scale-75 transform px-4 pb-1 pt-1 text-sm text-slate-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:text-sm lg:text-lg"
+                    >
+                      Email address
+                    </label>
+                  </div>
+                  <div>
+                    <button
+                      className="md:text-md flex h-10 items-center rounded bg-red-600  px-2 text-sm font-bold text-white hover:bg-red-700  md:h-12 md:px-4 lg:h-14 lg:px-6 lg:text-lg"
+                      type="submit"
+                    >
+                      Get Started <MdKeyboardArrowRight />
+                    </button>
+                  </div>
+                </form>
+              )}
             </div>
-          </>
-
-          {IS_LOGGED_IN && USER_DATA.plan === "NONE" ? (
-            <Link
-              to="/signup/choose"
-              className=" mt-4 rounded bg-red-600 px-6 py-3 align-middle text-2xl font-bold text-white hover:bg-red-700"
-            >
-              Finish signup
-              <MdKeyboardArrowRight />
-            </Link>
-          ) : null}
+          )}
         </section>
       </div>
     </Layout>
