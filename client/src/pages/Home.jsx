389ca39e1cbeaf071ac20d957f9603629db7b38d
sub1120 <subhamsahu1120@gmail.com>
2023-07-10 12:50:18 +0530

    made home ui responsive, added admin icon, removed dead code from header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import validator from "email-validator";
 import toast from "react-hot-toast";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 // components
 import Accordian from "../components/accordian/Accordian";
@@ -46,30 +47,32 @@ const Home = () => {
     <Layout>
       <div className="relative bg-[#000000] text-white">
         <section className="m-auto w-full">
-          <div className="bg-repeat-no h-[43.75rem] bg-netflix-home bg-cover">
+          <div className="bg-repeat-no h-[35rem] bg-netflix-home bg-cover md:h-[45rem]">
             <div
-              className="bg-[rgb(0 0   0 / 40%)] flex h-[43.75rem] w-full items-center justify-center "
+              className="bg-[rgb(0 0   0 / 40%)] flex h-full w-full items-center justify-center "
               style={{
                 backgroundImage:
                   "linear-gradient(to top, rgba(0, 0, 0, 0.9) 0, rgba(0, 0, 0, 0.4) 60%, rgba(0, 0, 0, 0.9) 100%)",
               }}
             >
-              <div className="flex w-auto flex-col items-center space-y-4">
-                <h1 className="sm:4xl text-2xl font-bold md:text-5xl">
-                  Unlimited movies, TV shows and more
-                </h1>
-                <p className="text-xl font-semibold md:text-2xl">
-                  Watch anywhere. Cancel anytime.
-                </p>
-                <p className="text-bold text-xl md:text-xl">
-                  Ready to watch? Enter your email to create or restart your
-                  membership.
-                </p>
+              <div className="space-y-4">
+                <div className="flex w-auto flex-col items-center space-y-4 px-2 text-center">
+                  <h1 className="text-2xl font-bold sm:text-4xl md:text-5xl">
+                    Unlimited movies, TV shows and more
+                  </h1>
+                  <p className="text-md font-semibold sm:text-xl md:text-2xl">
+                    Watch anywhere. Cancel anytime.
+                  </p>
+                  <p className="text-md font-semibold sm:text-xl md:text-2xl">
+                    Ready to watch? Enter your email to create or restart your
+                    membership.
+                  </p>
+                </div>
 
                 {loading ? (
                   "loading"
                 ) : (
-                  <>
+                  <div className="flex justify-center">
                     {IS_LOGGED_IN && USER_DATA.plan === "NONE" ? (
                       <Link to="/signup/choose">
                         <button
@@ -77,49 +80,49 @@ const Home = () => {
                           className="mt-5 rounded bg-red-600 px-4 py-2 align-middle text-lg font-medium text-white hover:bg-red-700"
                         >
                           Finish signUp
-                          <StartIcon />
+                          <MdOutlineKeyboardArrowRight />
                         </button>
                       </Link>
                     ) : (
                       <form
-                        className="flex flex-wrap md:space-x-4"
+                        className="flex w-[80%] flex-col items-center justify-center gap-2 md:flex-row md:gap-4 lg:gap-6"
                         onSubmit={(e) => handleIsUserExist(e)}
                       >
-                        <div className="max-w-80 group relative z-0 mb-6 rounded border-2 bg-black text-sm opacity-75 md:w-96">
+                        <div className="group relative z-0 w-full rounded border-2 bg-black text-sm opacity-75 sm:w-96">
                           <input
                             type="email"
                             name="email"
                             id="floating_email"
-                            className="peer block w-full appearance-none bg-transparent p-4 focus:border-blue-600 focus:outline-none focus:ring-0"
+                            className="peer block w-full appearance-none bg-transparent p-3 focus:border-blue-600 focus:outline-none focus:ring-0 md:p-4 lg:p-5"
                             placeholder=" "
                             required
                           />
                           <label
                             htmlFor="floating_email"
-                            className="absolute top-2 -z-10 origin-[0] -translate-y-6 scale-75 transform px-4 pb-1 pt-1 text-xl text-slate-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:text-sm"
+                            className="md:text-md absolute top-2 -z-10 origin-[0] -translate-y-6 scale-75 transform px-4 pb-1 pt-1 text-sm text-slate-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:text-sm lg:text-lg"
                           >
                             Email address
                           </label>
                         </div>
-                        <div>
+                        <div className="flex h-12 items-center rounded bg-red-600 px-2 text-2xl  font-bold text-white hover:bg-red-700 md:px-4 lg:px-6">
                           <button
                             type="submit"
-                            className="rounded bg-red-600 px-6 py-3 align-middle text-2xl font-bold text-white hover:bg-red-700"
+                            className="md:text-md text-sm lg:text-lg"
                           >
                             Get Started
-                            <StartIcon />
                           </button>
+                          <MdOutlineKeyboardArrowRight />
                         </div>
                       </form>
                     )}
-                  </>
+                  </div>
                 )}
               </div>
             </div>
           </div>
         </section>
 
-        <section className="mx-auto max-w-[80vw]">
+        <section className="mx-auto max-w-[90%] space-y-2 md:max-w-[85%] lg:max-w-[80%]">
           {features &&
             features.map((item, index) => {
               return (
@@ -134,8 +137,10 @@ const Home = () => {
             })}
         </section>
 
-        <section className="mx-auto max-w-[80vw] space-y-4">
-          <h2 className="font-bold">Frequently Asked Questions</h2>
+        <section className="mx-auto max-w-[90%] space-y-4 py-10 md:max-w-[85%] lg:max-w-[80%]">
+          <h2 className="text-xl font-bold md:text-3xl lg:text-5xl">
+            Frequently Asked Questions
+          </h2>
           <Accordian>
             {faqs.map((item) => {
               return (
