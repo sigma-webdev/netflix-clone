effad67eed5726c3be2d2a762a6a16250f89512d
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-07-11 16:36:13 +0530

    fix: fixed route going back to browse on refresh

--- client/src/helpers/auth/RequireAuth.jsx
+++ client/src/helpers/auth/RequireAuth.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
@@ -7,14 +8,31 @@ const RequireAuth = ({ allowedRoles }) => {
   const userData = useSelector((state) => state.auth.userData);
   const plan = userData?.plan;
   const role = userData?.role;
+
+  useEffect(() => {
+    if (!location.pathname.includes("/signin")) {
+      localStorage.setItem("lastLocation", location.pathname);
+    }
+  }, [location]);
+  const lastLocation = localStorage.getItem("lastLocation");
+  // localStorage.removeItem("lastLocation");
+
   return isLoggedIn &&
     (role === "USER" || role === "ADMIN") &&
     allowedRoles.includes(role) ? (
     <Outlet />
   ) : isLoggedIn ? (
-    <Navigate to={"/denied"} state={{ from: location }} replace />
+    <Navigate
+      to={"/denied"}
+      state={{ from: lastLocation || location }}
+      replace
+    />
   ) : (
-    <Navigate to={"/signin"} state={{ from: location }} replace />
+    <Navigate
+      to={"/signin"}
+      state={{ from: lastLocation || location }}
+      replace
+    />
   );
 };
 
