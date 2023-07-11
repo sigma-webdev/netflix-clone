a8872bacaa04558c2432732c0c11e350718e7431
sub1120 <subhamsahu1120@gmail.com>
2023-07-11 19:03:42 +0530

    refactored video controller and video shimmer

--- client/src/components/shimmer/VideoShimmer.jsx
+++ client/src/components/shimmer/VideoShimmer.jsx
@@ -1,7 +1,5 @@
 import { RiFullscreenFill } from "react-icons/ri";
 import PreviewShimmer from "./PreviewShimmer";
-import { IconContext } from "react-icons/lib";
-import { TbPlayerSkipForward } from "react-icons/tb";
 import { BsFillPlayFill, BsVolumeUp } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { MdKeyboardBackspace } from "react-icons/md";
@@ -9,55 +7,68 @@ import { AiOutlineBackward, AiOutlineForward } from "react-icons/ai";
 
 const VideoShimmer = () => {
   return (
-    <div className="relative h-screen bg-netflix-black">
-      <div className="absolute z-10 m-auto box-border h-screen w-[98%] opacity-0 transition delay-150 duration-300 ease-out hover:opacity-100">
-        <div className="absolute left-4 top-4">
-          <IconContext.Provider value={{ size: "40px", color: "white" }}>
-            <Link to="/browse">
-              <MdKeyboardBackspace />
-            </Link>
-          </IconContext.Provider>
+    <div className="relative h-screen w-full bg-netflix-black">
+      {/* video */}
+      <div className="absolute h-screen w-full">
+        <PreviewShimmer />
+      </div>
+
+      <div className="absolute z-10 box-border flex h-screen w-full flex-col justify-between p-4 opacity-0 transition delay-150 duration-300 ease-out hover:opacity-100">
+        {/* back button */}
+        <div className="top-4">
+          <Link to="/browse">
+            <MdKeyboardBackspace className="text-3xl text-white md:text-4xl lg:text-5xl" />
+          </Link>
         </div>
-        <div className="absolute bottom-16 left-4 h-1 w-full rounded bg-white">
-          <div className={`h-full w-[0px] rounded bg-slate-500`}></div>
-        </div>
-        <div className="absolute bottom-4 left-4 flex w-full items-center justify-between gap-4">
-          <div className="flex gap-4">
-            <IconContext.Provider value={{ size: "40px", color: "white" }}>
-              <div className="flex cursor-pointer items-center">
-                <BsFillPlayFill />
-              </div>
-              <div className="flex cursor-pointer items-center">
-                <AiOutlineBackward />
-              </div>
-              <div className="flex cursor-pointer items-center">
-                <AiOutlineForward />
-              </div>
-              <div className="flex cursor-pointer items-center">
-                <div>
-                  <BsVolumeUp />
-                </div>
-                <div>
-                  <input type="range" min="0" max="1" step=".1" />
-                </div>
-              </div>
-            </IconContext.Provider>
+
+        <div className="w-full space-y-2">
+          {/* video progress bar */}
+          <div className="h-1 rounded bg-white">
+            <div className={`h-full w-[0px] rounded bg-slate-500`}></div>
           </div>
 
-          <div className="flex gap-4">
-            <IconContext.Provider value={{ size: "40px", color: "white" }}>
-              <div className="cursor-pointer">
-                <TbPlayerSkipForward />
+          {/* video controls */}
+          <div className="flex w-full items-center justify-between gap-2 md:gap-3 lg:gap-4">
+            <div className="flex gap-2 md:gap-3 lg:gap-4">
+              {/* play/pause button */}
+              <button className="flex cursor-pointer items-center">
+                <BsFillPlayFill className="text-3xl text-white md:text-4xl lg:text-5xl" />
+              </button>
+
+              {/* playback button */}
+              <button className="flex cursor-pointer items-center">
+                <AiOutlineBackward className="text-3xl text-white md:text-4xl lg:text-5xl" />
+              </button>
+
+              {/* playforward button */}
+              <button className="flex cursor-pointer items-center">
+                <AiOutlineForward className="text-3xl text-white md:text-4xl lg:text-5xl" />
+              </button>
+
+              {/* volume button */}
+              <div className="group flex cursor-pointer items-center">
+                <button>
+                  <BsVolumeUp className="text-3xl text-white md:text-4xl lg:text-5xl" />
+                </button>
+                <div className="opacity-0 group-hover:opacity-100">
+                  <input
+                    type="range"
+                    min="0"
+                    max="1"
+                    step=".1"
+                    className="w-20 md:w-24 lg:w-28"
+                  />
+                </div>
               </div>
-              <div className="cursor-pointer">
-                <RiFullscreenFill />
-              </div>
-            </IconContext.Provider>
+            </div>
+
+            {/* fullscreen button */}
+            <div className="cursor-pointer">
+              <RiFullscreenFill className="text-3xl text-white md:text-4xl lg:text-5xl" />
+            </div>
           </div>
         </div>
       </div>
-
-      <PreviewShimmer />
     </div>
   );
 };
--- client/src/components/video/VideoController.jsx
+++ client/src/components/video/VideoController.jsx
@@ -1,21 +1,19 @@
-import { IconContext } from "react-icons/lib";
-import { MdKeyboardBackspace } from "react-icons/md";
-import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
-import { BsVolumeUp } from "react-icons/bs";
-import { TbPlayerSkipForward } from "react-icons/tb";
-import { RiFullscreenFill } from "react-icons/ri";
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineBackward, AiOutlineForward } from "react-icons/ai";
+import { BsFillPlayFill, BsFillPauseFill, BsVolumeUp } from "react-icons/bs";
+import { RiFullscreenFill } from "react-icons/ri";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 const VideoController = ({ contentURL, thumbnailURL }) => {
   const videoRef = useRef(null);
   const progressRef = useRef(null);
   const progressBarRef = useRef(null);
 
-  const [isVideoPlaying, setIsVideoPlaying] = useState(true);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const [volume, setVolume] = useState();
 
+  // play and pause media
   function playPauseMedia() {
     const media = videoRef.current;
 
@@ -31,38 +29,39 @@ const VideoController = ({ contentURL, thumbnailURL }) => {
   let timeoutFwd;
   let timeoutRwd;
 
+  // skip video backward by 10 sec
   function mediaBackward() {
     clearTimeout(timeoutFwd);
     timeoutRwd = setTimeout(windBackward, 200);
   }
-
-  function mediaForward() {
-    clearTimeout(timeoutRwd);
-    timeoutFwd = setTimeout(windForward, 200);
-  }
-
   function windBackward() {
     const media = videoRef.current;
 
     if (media.currentTime <= 10) {
-      clearInterval(timeoutRwd);
+      clearTimeout(timeoutRwd);
       stopMedia();
     } else {
       media.currentTime -= 10;
     }
   }
 
+  // skip video forward by 10 sec
+  function mediaForward() {
+    clearTimeout(timeoutRwd);
+    timeoutFwd = setTimeout(windForward, 200);
+  }
   function windForward() {
     const media = videoRef.current;
 
     if (media.currentTime >= media.duration - 3) {
-      clearInterval(timeoutFwd);
+      clearTimeout(timeoutFwd);
       stopMedia();
     } else {
       media.currentTime += 3;
     }
   }
 
+  // stop video
   function stopMedia() {
     const media = videoRef.current;
 
@@ -72,7 +71,8 @@ const VideoController = ({ contentURL, thumbnailURL }) => {
     setIsVideoPlaying(false);
   }
 
-  function setTime() {
+  // update video progress
+  function videoProgressHanlder() {
     const media = videoRef.current;
     const timerWrapper = progressBarRef.current;
     const timerBar = progressRef.current;
@@ -83,7 +83,8 @@ const VideoController = ({ contentURL, thumbnailURL }) => {
     timerBar.style.width = `${barLength}px`;
   }
 
-  function toggleFullScreen() {
+  // fullscreen video
+  function fullScreenHandler() {
     if (!document.fullscreenElement) {
       document.documentElement.requestFullscreen();
     } else if (document.exitFullscreen) {
@@ -91,95 +92,103 @@ const VideoController = ({ contentURL, thumbnailURL }) => {
     }
   }
 
+  // update video volume
   function volumeHandler(e) {
     setVolume(e.target.value);
     videoRef.current.volume = e.target.value;
   }
 
-  function mute() {
+  // mute video
+  function muteHandler() {
     videoRef.muted = !videoRef.muted;
   }
 
   return (
-    <div className="relative h-screen bg-netflix-black">
-      <div className="absolute z-10 m-auto box-border h-screen w-[98%] opacity-0 transition delay-150 duration-300 ease-out hover:opacity-100">
+    <div className="relative h-screen w-full bg-netflix-black">
+      {/* video */}
+      <video
+        ref={videoRef}
+        className="absolute h-screen w-full"
+        src={contentURL}
+        poster={thumbnailURL}
+        onEnded={stopMedia}
+        onTimeUpdate={videoProgressHanlder}
+      ></video>
+
+      <div className="absolute z-10 box-border flex h-screen w-full flex-col justify-between p-4 opacity-0 transition delay-150 duration-300 ease-out hover:opacity-100">
         {/* back button */}
-        <div className="absolute left-4 top-4">
+        <div className="top-4">
           <Link to="/browse">
-            <MdKeyboardBackspace className="text-5xl text-white" />
+            <MdKeyboardBackspace className="text-3xl text-white md:text-4xl lg:text-5xl" />
           </Link>
         </div>
-        <div
-          ref={progressBarRef}
-          className="absolute bottom-16 left-4 h-1 w-full rounded bg-white"
-        >
-          <div
-            ref={progressRef}
-            className={`h-full w-[0px] rounded bg-slate-500`}
-          ></div>
-        </div>
 
-        <div className="absolute bottom-2 left-4 flex w-full items-center justify-between gap-4">
-          <div className="flex gap-4">
+        <div className="w-full space-y-2">
+          {/* video progress bar */}
+          <div ref={progressBarRef} className="h-1 rounded bg-white">
             <div
-              onClick={playPauseMedia}
-              className="flex cursor-pointer items-center"
-            >
-              {!isVideoPlaying ? (
-                <BsFillPlayFill className="text-5xl text-white" />
-              ) : (
-                <BsFillPauseFill className="text-5xl text-white" />
-              )}
-            </div>
-            <div
-              onClick={mediaBackward}
-              className="flex cursor-pointer items-center"
-            >
-              <AiOutlineBackward className="text-5xl text-white" />
-            </div>
-            <div
-              onClick={mediaForward}
-              className="flex cursor-pointer items-center"
-            >
-              <AiOutlineForward className="text-5xl text-white" />
-            </div>
-            <div className="flex cursor-pointer items-center">
-              <div onClick={mute}>
-                <BsVolumeUp className="text-5xl text-white" />
-              </div>
-              <div>
-                <input
-                  type="range"
-                  min="0"
-                  max="1"
-                  step=".1"
-                  value={volume}
-                  onChange={volumeHandler}
-                />
-              </div>
-            </div>
+              ref={progressRef}
+              className={`h-full w-[0px] rounded bg-slate-500`}
+            ></div>
           </div>
 
-          <div className="flex gap-4">
-            <div className="cursor-pointer">
-              <TbPlayerSkipForward className="text-5xl text-white" />
+          {/* video controls */}
+          <div className="flex w-full items-center justify-between gap-2 md:gap-3 lg:gap-4">
+            <div className="flex gap-2 md:gap-3 lg:gap-4">
+              {/* play/pause button */}
+              <button
+                onClick={playPauseMedia}
+                className="flex cursor-pointer items-center"
+              >
+                {!isVideoPlaying ? (
+                  <BsFillPlayFill className="text-3xl text-white md:text-4xl lg:text-5xl" />
+                ) : (
+                  <BsFillPauseFill className="text-3xl text-white md:text-4xl lg:text-5xl" />
+                )}
+              </button>
+
+              {/* playback button */}
+              <button
+                onClick={mediaBackward}
+                className="flex cursor-pointer items-center"
+              >
+                <AiOutlineBackward className="text-3xl text-white md:text-4xl lg:text-5xl" />
+              </button>
+
+              {/* playforward button */}
+              <button
+                onClick={mediaForward}
+                className="flex cursor-pointer items-center"
+              >
+                <AiOutlineForward className="text-3xl text-white md:text-4xl lg:text-5xl" />
+              </button>
+
+              {/* volume button */}
+              <div className="group flex cursor-pointer items-center">
+                <button onClick={muteHandler}>
+                  <BsVolumeUp className="text-3xl text-white md:text-4xl lg:text-5xl" />
+                </button>
+                <div className="opacity-0 group-hover:opacity-100">
+                  <input
+                    type="range"
+                    min="0"
+                    max="1"
+                    step=".1"
+                    value={volume}
+                    onChange={volumeHandler}
+                    className="w-20 md:w-24 lg:w-28"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="cursor-pointer" onClick={toggleFullScreen}>
-              <RiFullscreenFill className="text-5xl text-white" />
+
+            {/* fullscreen button */}
+            <div className="cursor-pointer" onClick={fullScreenHandler}>
+              <RiFullscreenFill className="text-3xl text-white md:text-4xl lg:text-5xl" />
             </div>
           </div>
         </div>
       </div>
-
-      <video
-        ref={videoRef}
-        className="mx-auto h-full"
-        src={contentURL}
-        poster={thumbnailURL}
-        onEnded={stopMedia}
-        onTimeUpdate={setTime}
-        autoPlay
-      ></video>
     </div>
   );
 };
