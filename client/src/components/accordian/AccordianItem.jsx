6d9f41ea488dde1e8380ed77b46c2ba0f4c45cf2
sub1120 <subhamsahu1120@gmail.com>
2023-07-11 16:09:02 +0530

    added comments to accordian items

--- client/src/components/accordian/AccordianItem.jsx
+++ client/src/components/accordian/AccordianItem.jsx
@@ -7,6 +7,7 @@ const AccordianItem = ({
   answer,
   id,
 }) => {
+  // open and close accordian handler
   const tabHandler = () => {
     if (!isActive) {
       accordianHandler(id);
@@ -17,16 +18,18 @@ const AccordianItem = ({
 
   return (
     <li onClick={tabHandler}>
-      <h5 className="text-md sm:lg flex items-center justify-between bg-blue-950/80 px-6 py-4 hover:bg-blue-950/70 sm:py-5 md:py-6 md:text-2xl">
-        <button>{question}</button>
-        <span className="cursor-pointer">
-          {!isActive ? (
-            <AiOutlinePlus className="text-xl md:text-2xl lg:text-4xl" />
-          ) : (
-            <AiOutlineClose className="text-xl md:text-2xl lg:text-4xl" />
-          )}
-        </span>
+      {/* accordian heading */}
+      <h5 className="text-md sm:lg flex cursor-pointer items-center justify-between bg-blue-950/80 px-6 py-4 hover:bg-blue-950/70 sm:py-5 md:py-6 md:text-2xl">
+        {question}
+
+        {!isActive ? (
+          <AiOutlinePlus className="text-xl md:text-2xl lg:text-4xl" />
+        ) : (
+          <AiOutlineClose className="text-xl md:text-2xl lg:text-4xl" />
+        )}
       </h5>
+
+      {/* accordian description */}
       <p
         className={`${
           isActive ? "text-md bg-blue-950 p-6 sm:text-lg md:text-2xl" : "hidden"
