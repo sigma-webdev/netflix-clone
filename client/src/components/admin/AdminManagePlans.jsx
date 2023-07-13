cd72ec7d0ad813d9b443aa36e71d92e999e1feb6
maheshsarade001 <maheshsarade001@gmail.com>
2023-07-13 10:46:02 +0530

    M

--- client/src/components/admin/AdminManagePlans.jsx
+++ client/src/components/admin/AdminManagePlans.jsx
@@ -14,7 +14,6 @@ const AdminManagePlans = () => {
   const allPlans = useSelector((state) => state.plans.allPlans);
   const updateLoader = useSelector((state) => state.plans.updateLoader);
   const loading = useSelector((state) => state.plans.loading);
-  console.log(allPlans, "state");
 
   const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
@@ -239,7 +238,7 @@ const AdminManagePlans = () => {
                   >
                     <td className="text-center">{index + 1}</td>
                     <td className="text-center">{plan.planName}</td>
-                    <td className="text-center">{plan.amount}</td>
+                    <td className="text-center">₹ {plan.amount}</td>
                     <td className="px-10 py-2 text-start">
                       {plan.description}
                     </td>
