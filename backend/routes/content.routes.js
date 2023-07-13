b39320507dd50950ae874db4f4f19918a80cd5cf
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-07-13 18:29:08 +0530

    contentLikes to contentLikesAndDisLikes

--- backend/routes/content.routes.js
+++ backend/routes/content.routes.js
@@ -6,7 +6,7 @@ const {
   getContentById,
   deleteContentById,
   updateContentById,
-  contentLikes,
+  contentLikesAndDisLikes,
 } = require("../controllers/content.controller.js");
 const jwtAuth = require("../middleware/jwtAuth.js");
 const authorizeRoles = require("../middleware/authorizeRoles.js");
@@ -20,7 +20,7 @@ contentRoute
     jwtAuth,
     authorizeRoles("ADMIN", "USER"),
     checkUserSubscription,
-    contentLikes
+    contentLikesAndDisLikes
   );
 
 // create and get content
