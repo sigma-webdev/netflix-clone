8939f391378f3898d938855d532f684bda9d6c4e
Nasikh CL <nasikcl@gmail.com>
2023-07-14 16:28:49 +0530

    renamed functions/slice

--- client/src/store/config.js
+++ client/src/store/config.js
@@ -3,13 +3,13 @@ import contentReducer from "./contentSlice";
 import userReducer from "./adminUserSlice";
 import authReducer from "./authSlice.js";
 import razorpayReducer from "./razorpaySlice";
-import adminSlice from "./adminManageContentSlice";
+import adminManageContentsSlice from "./adminManageContentsSlice";
 import adminPlansSlice from "./adminPlansSlice";
 import dashboardSlice from "./dashboardSlice";
 
 export const store = configureStore({
   reducer: {
-    admin: adminSlice,
+    adminManageContents: adminManageContentsSlice,
     content: contentReducer,
     user: userReducer,
     auth: authReducer,
